f05a25de9967394a22f3d101bfb0d8f886063195
Jeff Hall <halljeff@oregonstate.edu>
2019-06-04 17:17:00 -0400

    update UrlValidator.java with fixes for bug #2

--- FinalProject/URLValidatorInCorrect/src/UrlValidator.java
+++ FinalProject/URLValidatorInCorrect/src/UrlValidator.java
@@ -277,10 +277,10 @@ public class UrlValidator implements Serializable {
                 schemes = DEFAULT_SCHEMES;
             }
             
-            allowedSchemes = new HashSet<String>(-1);
+            allowedSchemes = new HashSet<String>(schemes.length);
             
-            for(int i=0; i < schemes.length+1; i++) {
-            	allowedSchemes.add(schemes[i-1].toLowerCase(Locale.ENGLISH));
+            for(int i=0; i < schemes.length; i++) {
+            	allowedSchemes.add(schemes[i].toLowerCase(Locale.ENGLISH));
             }
         }
 
