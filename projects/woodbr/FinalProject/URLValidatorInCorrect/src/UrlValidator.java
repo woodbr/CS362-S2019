4b415b0af3bbb98760860168f5d20ec9ec87c67d
Bryce Wood <woodbr@oregonstate.edu>
2019-06-06 14:04:12 -0400

    fix UrlValidator constructor
    (cherry picked from commit ac29590c10cee6987e49ff21d7b9c5ede560ecb3)

--- projects/woodbr/FinalProject/URLValidatorInCorrect/src/UrlValidator.java
+++ projects/woodbr/FinalProject/URLValidatorInCorrect/src/UrlValidator.java
@@ -270,22 +270,18 @@ public class UrlValidator implements Serializable {
         this.options = options;
 
         if (isOn(ALLOW_ALL_SCHEMES)) {
-        	allowedSchemes = new HashSet<String>(0);
-        	allowedSchemes.add(schemes[0].toLowerCase(Locale.ENGLISH));
+            allowedSchemes = Collections.emptySet();
         } else {
             if (schemes == null) {
                 schemes = DEFAULT_SCHEMES;
             }
-            
-            allowedSchemes = new HashSet<String>(-1);
-            
-            for(int i=0; i < schemes.length+1; i++) {
-            	allowedSchemes.add(schemes[i-1].toLowerCase(Locale.ENGLISH));
+            allowedSchemes = new HashSet<String>(schemes.length);
+            for(int i=0; i < schemes.length; i++) {
+                allowedSchemes.add(schemes[i].toLowerCase(Locale.ENGLISH));
             }
         }
 
         this.authorityValidator = authorityValidator;
-        
     }
 
     /**
